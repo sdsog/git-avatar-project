a5a17c8c489bd8282c209db7420bd5dbc8593d80
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-03 19:17:14 +0000

    exercise 3 finished

---
+++ getAndPrintHTML-2.js
@@ -0,0 +1,32 @@
+var https = require('https'); 
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+function getAndPrintHTML(options) {
+
+  https.get(options, function (response) {
+
+  	response.setEncoding('utf8');
+	//empty string variable
+  	var buffer = '';
+
+    //when data chunk received, push it into buffer
+  	response.on('data', function(chunk) {
+  		buffer += chunk;
+  	}); 
+
+    //when all data is received, console log the buffer
+  	response.on('end', function() {
+    console.log(buffer);
+    });
+
+  });
+  /* Add your code here */
+
+}
+
+
+getAndPrintHTML(requestOptions);
